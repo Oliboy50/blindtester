3d20e7f93748c294d38cf78462de277ce2ade61b
Oliver THEBAULT <Oliboy50@users.noreply.github.com>
2019-07-28 12:29:57 +0200

    feat(filesStorage): add isValidFileStorage method (#11)

--- api/src/core/fileFinder.js
+++ api/src/core/fileFinder.js
@@ -1,10 +1,6 @@
 const uniqid = require('uniqid');
-const { promisify } = require('util');
-const fs = require('fs');
-const access = promisify(fs.access);
-const { storeNewFileForUrlAndId } = require('./filesStorage');
+const { storeNewFileForUrlAndId, isValidFileStorage } = require('./filesStorage');
 const { getFiles, saveFiles } = require('./database');
-const { FILES_STORAGE_TYPE_FILESYSTEM } = require('../../config/const');
 
 const findExistingFile = async (files, {id, url}) => {
   const matcherField = id ? 'id' : 'url';
@@ -17,17 +13,11 @@ const findExistingFile = async (files, {id, url}) => {
 
   const file = files[fileIndex];
 
-  // check if file storage is still valid
-  // @TODO check if backblazeb2 file storage is valid
-  if (file.storage.type === FILES_STORAGE_TYPE_FILESYSTEM) {
-    try {
-      await access(file.storage.path, fs.constants.R_OK);
-    } catch (e) {
-      // replace existing file with a valid one
-      file.storage = await storeNewFileForUrlAndId(file.url, file.id);
-      files.splice(fileIndex, 1, file);
-      await saveFiles(files);
-    }
+  if (!(await isValidFileStorage(file.storage))) {
+    // replace existing file with a valid one
+    file.storage = await storeNewFileForUrlAndId(file.url, file.id);
+    files.splice(fileIndex, 1, file);
+    await saveFiles(files);
   }
 
   return file;
--- api/src/core/filesStorage/index.js
+++ api/src/core/filesStorage/index.js
@@ -1,6 +1,7 @@
 const config = require('../../../config');
-const { storeNewFileForUrlAndId } = require(`./${config.filesStorage.type}`);
+const { storeNewFileForUrlAndId, isValidFileStorage } = require(`./${config.filesStorage.type}`);
 
 module.exports = {
   storeNewFileForUrlAndId,
+  isValidFileStorage,
 };
