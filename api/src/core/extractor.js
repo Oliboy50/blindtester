3b12ff70c6aaac2743309aa1625d7f29783a1cb7
Oliboy50 <oliver.thebault@gmail.com>
2020-07-12 20:33:30 +0200

    fix(validate): fix file duration on heroku

--- api/src/core/extractor.js
+++ api/src/core/extractor.js
@@ -1,5 +1,5 @@
 const { resolve } = require('path');
-const { spawn } = require('child_process');
+const { exec, spawn } = require('child_process');
 const config = require('../../config');
 const { FILES_STORAGE_TYPE_FILESYSTEM } = require('../../config/const');
 const { convertDurationFromTimecodeToNumberOfSeconds } = require('./durationConverter');
@@ -25,7 +25,7 @@ module.exports = {
           url,
         ],
         {
-          stdio: ['ignore', 'ignore', 'ignore'],
+          stdio: 'ignore',
           detached: true,
         },
       );
@@ -54,37 +54,24 @@ module.exports = {
   },
   async getFileDurationInSecondsFromUrl(url) {
     const durationAsTimecode = await new Promise((resolve, reject) => {
-      const extractionCommand = spawn(
-        'youtube-dl',
-        [
-          '--skip-download',
-          '--get-duration',
-          url,
-        ],
-        {
-          stdio: ['ignore', 'pipe', 'ignore'],
-          detached: true,
-        },
-      );
-
-      extractionCommand.stdout.on('data', (data) => {
-        resolve(String(data).replace(/\n/g, ''));
-      });
-
-      extractionCommand.on('error', (err) => {
-        const errorMessage = `Failed to compute file duration for url [${url}]. Error: ${err.message}`;
-        // eslint-disable-next-line no-console
-        console.log(errorMessage);
-        reject(new Error(errorMessage));
-      });
-
-      extractionCommand.on('exit', (code) => {
-        if (code !== 0) {
-          const errorMessage = `File duration computing exit code [${code}] for url [${url}]`;
+      exec(`youtube-dl --skip-download --get-duration ${url}`, (err, stdout, stderr) => {
+        if (stderr) {
+          const errorMessage = `Failed to compute file duration for url [${url}]. Error: ${String(stderr).trim()}`;
           // eslint-disable-next-line no-console
           console.log(errorMessage);
           reject(new Error(errorMessage));
+          return;
         }
+
+        if (err) {
+          const errorMessage = `Error while computing file duration for url [${url}]. Error: ${err.message}`;
+          // eslint-disable-next-line no-console
+          console.log(errorMessage);
+          reject(new Error(errorMessage));
+          return;
+        }
+
+        resolve(String(stdout).trim());
       });
     });
 
